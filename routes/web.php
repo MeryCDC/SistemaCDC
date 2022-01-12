c3e518d50d6f6dd470c3230a5a264ddaf78ecb5b
MeryCDC <desarrollo2@controldecarga.com>
2022-01-12 12:59:36 -0600

    Rutas Web

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,32 @@
 <?php
 
+//use App\Http\Controllers\BarCodeController;
+use App\Http\Controllers\EntradasBodegaController;
+use App\Http\Controllers\EntradasImportacionController;
+use App\Http\Controllers\SalidasImportacionController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [EntradasImportacionController::class, 'index'])->middleware('auth')->name('home');
+
+Route::group(['middleware' => 'auth'], function () 
+{
+    Route::get('/', [EntradasImportacionController::class , 'index'])->name('home');
+}); 
+
+/*|-------------------------------------------------------------------------- */
+
+Route::resource('/ingresos' , EntradasImportacionController::class );
+
+Route::resource('/entradas' , EntradasBodegaController::class );
+
+Route::get('/ingresos/{id}/guias', [EntradasImportacionController::class, 'show'])->name('ingresos.guias');
+
+Route::get('/etiqueta/{id}', [BarCodeController::class, 'index']);
+
+Route::resource('/salidas' , SalidasImportacionController::class );
+
+Route::get('/salidas/{id}/guias', [SalidasImportacionController::class, 'show'])->name('salidas.guias');
+
+Route::get('/entradas/export-excel', [EntradasBodegaController::class, 'exportExcel'])->name('entradas.excel');
