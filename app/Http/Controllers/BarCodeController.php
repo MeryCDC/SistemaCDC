de8b4b6439b01be3e4ddf6c90fcd72290030db7f
MeryCDC <desarrollo2@controldecarga.com>
2022-01-12 15:42:40 -0600

    Ruta BarCOde

--- app/Http/Controllers/BarCodeController.php
+++ app/Http/Controllers/BarCodeController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\entradas_bodegas;
 
 class BarCodeController extends Controller
 {
-    //
+    // index
+    public function index($id)
+    {
+        /* id */
+        $guia = entradas_bodegas::findOrFail($id);
+        return view('barcode.index', compact('guia'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-//use App\Http\Controllers\BarCodeController;
+use App\Http\Controllers\BarCodeController;
 use App\Http\Controllers\EntradasBodegasController;
 use App\Http\Controllers\EntradasImportacionesController;
 use App\Http\Controllers\SalidasImportacionsController;
@@ -15,18 +15,18 @@ Route::group(['middleware' => 'auth'], function ()
     Route::get('/', [EntradasImportacionesController::class , 'index'])->name('home');
 }); 
 
-/*|-------------------------------------------------------------------------- */
+/*| ---------------------------------------------------------------------------------------------------------------------------------------------------- */
 
-Route::resource('/ingresos' , EntradasImportacionesController::class );
+Route::resource('/ingresos' , EntradasImportacionesController::class )->middleware('auth');
 
-Route::resource('/entradas' , EntradasBodegasController::class );
+Route::resource('/entradas' , EntradasBodegasController::class )->middleware('auth');
 
-Route::get('/ingresos/{id}/guias', [EntradasImportacionesController::class, 'show'])->name('ingresos.guias');
+Route::get('/ingresos/{id}/guias', [EntradasImportacionesController::class, 'show'])->name('ingresos.guias')->middleware('auth');
 
 Route::get('/etiqueta/{id}', [BarCodeController::class, 'index']);
 
-Route::resource('/salidas' , SalidasImportacionsController::class );
+Route::resource('/salidas' , SalidasImportacionsController::class )->middleware('auth');
 
-Route::get('/salidas/{id}/guias', [SalidasImportacionsController::class, 'show'])->name('salidas.guias');
+Route::get('/salidas/{id}/guias', [SalidasImportacionsController::class, 'show'])->name('salidas.guias')->middleware('auth');
 
-Route::get('/entradas/export-excel', [EntradasBodegaController::class, 'exportExcel'])->name('entradas.excel');
+Route::get('/entradas/export-excel', [EntradasBodegaController::class, 'exportExcel'])->name('entradas.excel')->middleware('auth');
