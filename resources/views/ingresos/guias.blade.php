14c46c8c686cd99d75c2cd879e4f495dd5295195
MeryCDC <desarrollo2@controldecarga.com>
2022-01-14 15:27:08 -0600

    Limpieza

--- resources/views/ingresos/guias.blade.php
+++ resources/views/ingresos/guias.blade.php
@@ -1,31 +1,23 @@
 @extends('layouts.master')
-
 @section('css')
     <link href="{{ asset('css/table.css') }}" rel="stylesheet">
     <link rel="stylesheet" type="text/css"
         href="https://cdn.datatables.net/responsive/2.2.9/css/responsive.bootstrap4.min.css">
 @endsection
-
 @section('content')
     <div class="content-wrapper">
         <div class="row">
             <div class="col-sm-12">
                 <div class="home-tab">
+                    
                     <div class="d-sm-flex align-items-center justify-content-between border-bottom">
                         <ul class="nav nav-tabs" role="tablist">
                             <li class="nav-item">
                                 <h3 class="nav-link">Guias</h3>
-                                {{-- <a class="nav-link" id="home-tab" data-bs-toggle="tab" role="tab" aria-controls="overview" aria-selected="true">Ingreso #: {{ $id }} </a> --}}
                             </li>
                             <li class="nav-item">
                                 <h3 class="nav-link">Codigo de ingreso #: {{ $id }}</h3>
                             </li>
-                            {{-- <li class="nav-item">
-                            <a class="nav-link" id="contact-tab" data-bs-toggle="tab" href="#demographics" role="tab" aria-selected="false">Demographics</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link border-0" id="more-tab" data-bs-toggle="tab" href="#more" role="tab" aria-selected="false">More</a>
-                        </li> --}}
                         </ul>
                         <div>
                             <div class="btn-wrapper">
@@ -127,7 +119,7 @@
                                                     <td>{{ $guia->ancho }}</td>
                                                     <td>{{ $guia->peso }}</td>
                                                     <td>{{ $guia->tipo }}</td>
-                                                    <td>{{ $guia->peso_volumetrico }}</td>
+                                                    <td>{{ number_format($guia->peso_volumetrico, 2) }}</td>
                                                     <td>{{ $guia->volumen }}</td>
                                                     <td>{{ $guia->name }}</td>
                                                     <td>
@@ -144,99 +136,15 @@
                                                 </tr>
                                             @endforeach
                                         </tbody>
-                                        <tfoot>
-                                            <tr>
-                                                <th style="width:15px">ID</th>
-                                                <th>T/G/P</th>
-                                                <th>ID CDC</th>
-                                                <th>Alto</th>
-                                                <th>Largo</th>
-                                                <th>Ancho</th>
-                                                <th>Peso</th>
-                                                <th>Tipo</th>
-                                                <th>Peso volumetrico</th>
-                                                <th>Volumen</th>
-                                                <th>Ingreso</th>
-                                                <th>Acciones</th>
-                                            </tr>
-                                        </tfoot>
-                                    </table>
+                                    </table> 
                                 </div>
                             </div>
                         </div>
                     </div>
-
                 </div>
             </div>
         </div>
     </div>
-
-    <!-- modal para agregar nueva minuta -->
-    <div class="modal fade" id="ingreso" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-        aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Nueva Observacion</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <form action="{{ url('/minutas') }}" class="forms-sample" method="post">
-                        @csrf
-                        <div class="form-group">
-                            <label for="Nombre">{{ __('Objetivo:') }}</label>
-                            <input type="text" class="form-control @error('Objetivo') is-invalid @enderror" id="Objetivo"
-                                name="Objetivo" placeholder="Ingrese el objetivo de la minuta"
-                                value="{{ old('Objetivo') }}">
-                            @error('Objetivo')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
-                        <div class="form-group">
-                            <label for="Lugar_Reunion">Lugar de reunión:</label>
-                            <input type="text" class="form-control  @error('Lugar_Reunion') is-invalid @enderror"
-                                id="Lugar_Reunion" name="Lugar_Reunion" placeholder="Ingrese el lugar de reunión"
-                                value="{{ old('Lugar_Reunion') }}">
-                            @error('Lugar_Reunion')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-
-                        </div>
-                        <div class="form-group">
-                            <label for="Fecha_Prox_Reuniun">Fecha de proxima reunión:</label>
-                            <input type="date" class="form-control @error('Fecha_Prox_Reuniun') is-invalid @enderror"
-                                id="Fecha_Prox_Reuniun" name="Fecha_Prox_Reuniun"
-                                value="{{ old('Fecha_Prox_Reuniun') }}">
-                            @error('Fecha_Prox_Reuniun')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
-
-                        <div class="form-group">
-                            <input type="hidden" class="form-control" id="user_id" name="user_id"
-                                value=" {{-- {{ Auth::user()->id }} --}}">
-                            <input type="hidden" class="form-control" id="Estatus" name="Estatus" value="0">
-                            <input type="hidden" class="form-control" id="Fecha_Creacion" name="Fecha_Creacion"
-                                value="{{ date('Y-m-d H:i') }}">
-                        </div>
-
-                        <button type="submit" class="btn btn-primary me-2">
-                            <i class="mdi mdi-plus"></i> Agregar Minuta
-                        </button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Final modal para agregar nueva minuta -->
 @endsection
 
 @section('js')
@@ -259,10 +167,6 @@
 
         $(function() {
             $('#tabla_guias').DataTable({
-                /* dom: 'Bfrtip', */
-                /* "buttons": [
-                    { extend: 'excelHtml5', footer: true },
-                ], */
                 "order": [
                     [0, "desc"]
                 ],
@@ -272,7 +176,6 @@
                 }
             });
         });
-
         /* Modal para agregar un nuevo ingreso */
         $(function() {
             $('#ingreso').on('show.bs.modal', function(event) {
