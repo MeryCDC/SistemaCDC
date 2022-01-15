891552f7ebf75e3214ac9896b3947f8d8d65d4b3
MeryCDC <desarrollo2@controldecarga.com>
2022-01-15 13:24:24 -0600

    AGREGAR ID DE SALIDA

--- app/Http/Controllers/BarCodeController.php
+++ app/Http/Controllers/BarCodeController.php
@@ -11,7 +11,22 @@ class BarCodeController extends Controller
     public function index($id)
     {
         /* id */
-        $guia = entradas_bodegas::findOrFail($id);
-        return view('barcode.index', compact('guia'));
+        $guiaBodega = entradas_bodegas::join('int_impo_bods' , 'int_impo_bods.int_bod_id', '=', 'entradas_bodegas.id')
+        ->where('entradas_bodegas.id', '=', $id)
+        ->get();
+
+        //return response()->json($guiaBodega); 
+
+         //Obtengo todas las guias de la importacion seleccionada
+         /* $guiasImportaciones=int_impo_bods::join('entradas_importaciones' , 'int_impo_bods.int_impo_id', '=', 'entradas_importaciones.id')
+         ->join('entradas_bodegas' , 'int_impo_bods.int_bod_id', '=', 'entradas_bodegas.id')
+         ->join('users' , 'entradas_bodegas.user_id', '=', 'users.id')
+         ->select('entradas_bodegas.id' , 'entradas_bodegas.tgp' , 'entradas_bodegas.peso', 'entradas_bodegas.tipo', 'entradas_bodegas.largo', 'entradas_bodegas.id_cdc',
+         'entradas_bodegas.ancho', 'entradas_bodegas.alto', 'entradas_bodegas.peso_volumetrico', 'entradas_bodegas.volumen', 'users.name')
+         ->where('entradas_importaciones.id', '=', $id)
+         ->get(); */
+
+
+        return view('barcode.index', compact('guiaBodega'));
     }
 }
--- resources/views/barcode/index.blade.php
+++ resources/views/barcode/index.blade.php
@@ -1,4 +1,5 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html
+    PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0088)http://controldecarga.ecusmart.net/sistema/paginas/print_label2.php?codguia=SLW402000251 -->
 <html xmlns="http://www.w3.org/1999/xhtml">
 
@@ -6,6 +7,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <title>Etiqueta</title>
 </head>
+
 <body style="margin:0px; padding:0px; font-family:Verdana, Geneva, sans-serif;" onload="self.print();">
     <div style="margin:3px; padding:3px; text-align:center; vertical-align:middle;">
         <!--<table border="1" width="377" height="577">-->
@@ -20,45 +22,55 @@
                                         Etiqueta
                                     </td>
                                     <td style="text-align: right;">
-                                        <span style="font-size: 13px; font-weight: bold;"> {{ date('Y-m-d H:i') }} </span>
+                                        <span style="font-size: 13px; font-weight: bold;"> {{ date('Y-m-d H:i') }}
+                                        </span>
                                     </td>
                                 </tr>
                             </tbody>
                         </table>
                     </td>
                 </tr>
+                @foreach ($guiaBodega as $guia)
+                    <tr>
+                        <td align="left" style="padding-left: 15px;">
+                            <table>
+                                <tbody> 
+                                    <tr>
+                                        <td style="vertical-align: top; font-size: 20px;"><b>ID:</b> {{ $guia->id }}
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td style="vertical-align: top; font-size: 20px;"><b>Dimensiones:</b>
+                                            {{ $guia->alto }} x {{ $guia->largo }} x {{ $guia->ancho }} in</td>
+                                    </tr>
+                                    <tr>
+                                        <td style="vertical-align: top; font-size: 20px;"><b>Peso:</b>
+                                            {{ $guia->peso }} lb</td> 
+                                    </tr>
+                                    <tr>
+                                        <td style="vertical-align: top; font-size: 20px;"><b>Codigo de Ingreso:</b>
+                                            {{ $guia->int_impo_id }}</td> 
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </td>
+                    </tr>
+                    <tr style="border-bottom: 1px solid red">
+                        <td>
+                            <span style="font-size:28px; font-weight: bold;">
+                                ID: {{ $guia->id }}</span>
+                            <br>
+                            <img src="data:image/png;base64,{{ DNS1D::getBarcodePNG($guia->id, 'C39') }}"
+                                alt="barcode" /><br><br>
+                        </td>
+                    </tr>
+                @endforeach
                 <tr>
-                    <td align="left" style="padding-left: 15px;">
+                    <td>
                         <table>
                             <tbody>
                                 <tr>
-                                    <td style="vertical-align: top; font-size: 20px;"><b>ID:</b> {{ $guia->id }} </td>
-                                </tr>
-                                <tr>
-                                    <td style="vertical-align: top; font-size: 20px;"><b>Dimensiones:</b> {{ $guia->alto }} x {{ $guia->largo }} x {{ $guia->ancho }} in</td>
-                                </tr>
-                                <tr>
-                                    <td style="vertical-align: top; font-size: 20px;"><b>Peso:</b> {{ $guia->peso }} lb</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </td>
-
-                </tr>
-                <tr style="border-bottom: 1px solid red">
-                    <td>
-                        <span style="font-size:28px; font-weight: bold;">
-                            ID: {{ $guia->id }}</span>
-                        <br>
-                        <img src="data:image/png;base64,{{DNS1D::getBarcodePNG($guia->id, 'C39')}}" alt="barcode" /><br><br>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <table>
-                            <tbody>
-                                <tr>
-                                   {{--  --}}
+                                    {{--  --}}
                                 </tr>
                             </tbody>
                         </table>
@@ -70,7 +82,8 @@
                             <tbody>
                                 <tr>
                                     <td align="center" style="padding: 2px;">
-                                        <span style="font-size: 14px; font-weight: bold;">PAQUETE SIN IDENTIFICAR</span><br>
+                                        <span style="font-size: 14px; font-weight: bold;">PAQUETE SIN
+                                            IDENTIFICAR</span><br>
                                     </td>
                                 </tr>
                             </tbody>
@@ -79,7 +92,7 @@
                 </tr>
                 <tr>
                     <td>
-                       
+
                     </td>
                 </tr>
             </tbody>
