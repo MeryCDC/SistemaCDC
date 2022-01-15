6f626bb98e15d59c3428f41acbae2ad6285a9dcd
MeryCDC <desarrollo2@controldecarga.com>
2022-01-15 13:29:18 -0600

    lIMPIEZA CODIGO

--- app/Http/Controllers/SalidasBodegasController.php
+++ app/Http/Controllers/SalidasBodegasController.php
@@ -9,32 +9,16 @@ use Illuminate\Http\Request;
 
 class SalidasBodegasController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $idCDC = "";
@@ -50,7 +34,7 @@ class SalidasBodegasController extends Controller
             if (empty($guia->id_cdc)) {
                 return redirect()->route('salidas.guias' , $request->id_importacion)->with('messageVacio', 'Vacio');
             }  else {
-                $diferencia =  $peso_original - $request->peso;
+                $diferencia = $request->peso - $peso_original;
                 $datos = new salidas_bodegas();
                 $datos->peso_salida = $request->peso;
                 $datos->peso_diferencia = $diferencia;
@@ -76,46 +60,21 @@ class SalidasBodegasController extends Controller
         //return response()->json($idNuevo); 
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\salidas_bodegas  $salidas_bodegas
-     * @return \Illuminate\Http\Response
-     */
     public function show(salidas_bodegas $salidas_bodegas)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\salidas_bodegas  $salidas_bodegas
-     * @return \Illuminate\Http\Response
-     */
     public function edit(salidas_bodegas $salidas_bodegas)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\salidas_bodegas  $salidas_bodegas
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, salidas_bodegas $salidas_bodegas)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\salidas_bodegas  $salidas_bodegas
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(salidas_bodegas $salidas_bodegas)
     {
         //
